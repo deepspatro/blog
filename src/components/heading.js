fccb7cec1fe57ae485c7c5189645545644983ad8
Deepanjali <deepanjali.patro@gmail.com>
2019-09-28 20:07:33 +0530

    added styles for heading

--- src/components/heading.js
+++ src/components/heading.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { Button, AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+
+const useStyles = makeStyles((theme) => (
+    {
+        title:
+        {
+            flexGrow: 1
+        }
+    }
+)
+
+)
+
 
 function Header() {
+    const classes = useStyles();
     return (
-        <h1 className="heading">This is Blog</h1>
+        <AppBar>
+            <Toolbar>
+                <Typography className={classes.title} variant="h4">Blog</Typography>
+                <Button variant="outlined" color="inherit">Add</Button>
+            </Toolbar>
+
+        </AppBar>
     )
 }
 
