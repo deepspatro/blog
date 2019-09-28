0508b11498803705cfa3a02a3ff9c8be59371b85
Deepanjali <deepanjali.patro@gmail.com>
2019-09-28 18:19:43 +0530

    save files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+import data from './data';
+import Card from './components/card';
+
 function App() {
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      {
+        data.map((item,index) => (
+          <Card key={index} title={item.title} author={item.author.name} />
+        ))
+      }
     </div>
   );
 }
